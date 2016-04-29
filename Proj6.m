a4b4c012441b8363140fdd8fde7158d1c231ddd7
lengie <cookiestork@gmail.com>
2016-04-29 02:28:03 -0700

    Fixed size/length issue and got pdf function working

--- Proj6.m
+++ Proj6.m
@@ -9,15 +9,15 @@ function []=Proj6(n,m)
 
 x = [];
 for i=1:m
-    x = [x;rand(1,n)];
+    x = [x,rand(n,1)];
 end
 
 %Box-Muller, m must be even (or can set up where n must be even)
 z = [];
 for i=1:n 
     for j=1:2:m-1
-        z(j,i) = sqrt(-2*log(x(j,i)))*cos(2*pi*x(j+1,i));
-        z(j+1,i) = sqrt(-2*log(x(j,i)))*sin(2*pi*x(j+1,i));
+        z(i,j) = sqrt(-2*log(x(i,j)))*cos(2*pi*x(i,j+1));
+        z(i,j+1) = sqrt(-2*log(x(i,j)))*sin(2*pi*x(i,j+1));
     end;
 end;
 
@@ -30,8 +30,8 @@ V = cov(x);
 e = eig(V);
 prob=1;
 for k=1:n
-    prob = prod*(1/(sqrt(eig(k,k)))*exp(-(x'*(V^(-1))*x)/2));
+    prob = prob*(1/(sqrt(eig(k,k)))*exp(-(x*(V^(-1))*x')/2));
 end;
-prob = prob*1/((2*pi)^(n/2));
+prob = prob*(1/((2*pi)^(n/2)));
 
 end
